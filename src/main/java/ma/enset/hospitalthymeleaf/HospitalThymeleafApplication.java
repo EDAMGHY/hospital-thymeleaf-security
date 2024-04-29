ffa2d2d741a9a0d0008da95d39f660171a541160
Abdellah DAMRI <damriabdellah@gmail.com>
2024-04-29 21:01:02 +0100

    feat: adding jdbc user details

--- src/main/java/ma/enset/hospitalthymeleaf/HospitalThymeleafApplication.java
+++ src/main/java/ma/enset/hospitalthymeleaf/HospitalThymeleafApplication.java
@@ -6,24 +6,55 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
+import javax.sql.DataSource;
 import java.util.Date;
 
 @SpringBootApplication
-public class HospitalThymeleafApplication implements CommandLineRunner {
-
-    @Autowired
-    private PatientRepository patientRepository;
+public class HospitalThymeleafApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HospitalThymeleafApplication.class, args);
     }
 
+    @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
 
-    @Override
-    public void run(String... args) throws Exception {
-//        patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 123));
-//        patientRepository.save(new Patient(null, "Yassine", new Date(), true, 765));
-//        patientRepository.save(new Patient(null, "Hamza", new Date(), false, 980));
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    CommandLineRunner start(PatientRepository patientRepository) {
+        return args -> {
+            patientRepository.save(new Patient(null, "Mohammed", new Date(), false, 123));
+            patientRepository.save(new Patient(null, "Yassine", new Date(), true, 765));
+            patientRepository.save(new Patient(null, "Hamza", new Date(), false, 980));
+        };
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager) {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return args -> {
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user22").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("admin2").password(passwordEncoder.encode("1234")).roles("USER", "ADMIN").build()
+            );
+        };
     }
 }
--- src/main/java/ma/enset/hospitalthymeleaf/security/SecurityConfig.java
+++ src/main/java/ma/enset/hospitalthymeleaf/security/SecurityConfig.java
@@ -9,7 +9,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 
 @Configuration
@@ -17,12 +20,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
 
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
 
-    @Bean
+    //    @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager(PasswordEncoder passwordEncoder) {
         String encodedPassword = passwordEncoder.encode("1234");
         System.out.println(encodedPassword);
